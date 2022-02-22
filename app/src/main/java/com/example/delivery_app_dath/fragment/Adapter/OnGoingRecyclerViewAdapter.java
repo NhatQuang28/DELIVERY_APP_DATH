8cd3c9612515b94440a9aed714625182fe239efb
NhatQuang28 <nguyenvnhatquang@gmail.com>
2022-02-22 21:49:24 +0700

    commit

---
+++ app/src/main/java/com/example/delivery_app_dath/fragment/Adapter/OnGoingRecyclerViewAdapter.java
@@ -0,0 +1,7 @@
+package com.example.delivery_app_dath.fragment.Adapter;
+
+//public class OnGoingRecyclerViewAdapter extends RecyclerView.Adapter<OnGoingRecyclerViewAdapter.OnGoingViewHolder> {
+//    public class OnGoingViewHolder(ArrayList<Vehicle> listVehicle, IClickItemVehicleListener listener) {
+//    }
+//
+//}
